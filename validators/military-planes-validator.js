4116fac3edba5d4fe3054fc038ef8af7c9126f73
Alina_Zhykharieva <alina_zhykharieva@epam.com>
2021-02-22 10:28:28 +0200

    Validator fixes

--- validators/military-planes-validator.js
+++ validators/military-planes-validator.js
@@ -4,13 +4,13 @@ const MilitaryType = require('../types/military-type');
 class MilitaryPlanesValidator {
     validateThatMilitaryPlanesHaveTransportType(airport) {
         airport.transportMilitaryPlanes.forEach((plane) => {
-            expect(plane.militaryType).to.deep.equal(MilitaryType.TRANSPORT);
+            expect(plane.militaryType).to.equal(MilitaryType.TRANSPORT);
         });
     }
 
     validateThatBomberTypeExist(airport) {
         airport.bomberMilitaryPlanes.forEach((plane) => {
-            expect(plane.militaryType).to.deep.equal(MilitaryType.BOMBER);
+            expect(plane.militaryType).to.equal(MilitaryType.BOMBER);
         });
     }
 }
